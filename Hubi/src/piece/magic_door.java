c6b07538510441ae005e4626177fc9f4dd381e74
unknown <garuwriters@gmail.com>
2024-01-19 12:32:16 +0100

    first commit

---
+++ Hubi/src/piece/magic_door.java
@@ -0,0 +1,11 @@
+package piece;
+
+public class magic_door extends Piece {
+
+	public magic_door( int col, int row) {
+		super( col, row);
+		// TODO Auto-generated constructor stub
+		image = getImage("/piece/magic_door");
+	}
+
+}
---
+++ Hubi/src/piece/nothubi.java
@@ -0,0 +1,11 @@
+package piece;
+
+public class nothubi extends Piece {
+
+	public nothubi( int col, int row) {
+		super( col, row);
+		// TODO Auto-generated constructor stub
+		image = getImage("/piece/nothubi");
+	}
+
+}
